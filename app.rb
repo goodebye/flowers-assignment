2de477ae063f75f17ed465581eee0110e38f98ef
Brad <bradleywyros@gmail.com>
2018-12-08 16:12:50 -0500

    Added triggers and indices to app.rb

--- app.rb
+++ app.rb
@@ -5,10 +5,68 @@ require "sqlite3"
 
 $db = SQLite3::Database.open "flowers.db"
 
+$db.execute %{CREATE INDEX location
+	        ON SIGHTINGS(LOCATION);
+
+		CREATE INDEX name 
+		ON SIGHTINGS(NAME);
+
+		CREATE INDEX person
+		ON SIGHTINGS(PERSON);
+
+		CREATE INDEX sighted
+		ON SIGHTINGS(SIGHTED);
+		
+		CREATE TRIGGER flower_update
+		AFTER UPDATE OF COMNAME ON FLOWERS
+		BEGIN
+		UPDATE SIGHTINGS
+		SET NAME = NEW.COMNAME
+		WHERE NAME = OLD.COMNAME;
+		END;
+
+		CREATE TRIGGER no_location
+		BEFORE INSERT ON SIGHTINGS
+		BEGIN
+		SELECT CASE
+		WHEN ((SELECT FEATURES.LOCATION
+		      FROM FEATURES
+		      WHERE FEATURES.LOCATION = NEW.LOCATION) IS NULL) 
+		      THEN RAISE (ABORT, 'Location is not found')
+		END;
+		END;
+
+		CREATE TRIGGER no_flower
+		BEFORE INSERT ON SIGHTINGS
+		BEGIN
+		SELECT CASE
+		WHEN ((SELECT COMNAME
+		      FROM FLOWERS
+		      WHERE COMNAME = NEW.NAME OR GENUS || ' ' || SPECIES = NEW.NAME) IS NULL) 
+		      THEN RAISE (ABORT, 'Flower is not found')
+		END;
+		END;
+
+		CREATE TRIGGER sci_name_change
+		AFTER INSERT ON SIGHTINGS
+		WHEN((SELECT GENUS || ' ' || SPECIES
+				  FROM FLOWERS
+				  WHERE GENUS =  substr(NEW.NAME, 1, instr(NEW.NAME, ' ') - 1) 
+				  AND SPECIES = substr(NEW.NAME, instr(NEW.NAME, ' ') + 1)) == NEW.NAME)
+				  BEGIN	
+				  UPDATE SIGHTINGS
+				  SET NAME = (SELECT COMNAME
+				              FROM FLOWERS
+				              WHERE GENUS || ' ' || SPECIES = NEW.NAME)
+				  WHERE NAME = NEW.NAME;
+		END;}
+
+
+
 def recent_sightings_query flower_name
-  sightings = $db.execute %{SELECT SIGHTED, PERSON, LOCATION, NAME
+  sightings = $db.execute %{SELECT SIGHTED, PERSON, LOCATION,
 			   FROM SIGHTINGS
-			   WHERE NAME = "#{flower_name}" or NAME = (select comname from flowers 				   WHERE genus || ' ' || species = "#{flower_name}")
+			   WHERE NAME = "#{flower_name}" or NAME = (SELECT COMNAME FROM FLOWERS 				   WHERE GENUS || ' ' || SPECIES = "#{flower_name}")
 			   ORDER BY SIGHTED DESC
 			   LIMIT 10;}
 
@@ -18,7 +76,7 @@ def recent_sightings_query flower_name
 end
 
 def get_flower_info_query flower_name
-  flower_info = $db.execute %{select * from flowers  WHERE genus || ' ' || species = "#{flower_name}" OR COMNAME = "#{flower_name}" }
+  flower_info = $db.execute %{SELECT * FROM FLOWERS WHERE GENUS || ' ' || SPECIES = "#{flower_name}" OR COMNAME = "#{flower_name}" }
   flower_info
 end
 
